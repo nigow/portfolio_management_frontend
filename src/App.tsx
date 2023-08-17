a3717395d0ca9eb0dc417c42ee4be2a0b7393510
tairi <tairi.goto@mail.utoronto.ca>
2023-08-17 16:49:04 +0900

    Removed chart.js registration from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
 import './App.css';
 import FoldableSidebar from './components/FoldableSidebar';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from "chart.js";
-import 'chart.js/auto'
 import CashFlow from "./components/CashFlow";
 import AccountManipulator from "./components/AccountManipulator"
 import TableOverview from "./components/TableOverview";
 
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-);
 
 function App() {
     return (
